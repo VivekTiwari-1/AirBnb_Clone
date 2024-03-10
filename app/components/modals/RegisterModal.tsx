8a6749fc153381c204ecc56849c1fafd55531ec8
Vivek kumar Tiwari <130132612+VivekTiwari-1@users.noreply.github.com>
2024-03-10 14:41:58 +0530

    Completed SignUp

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -7,6 +7,10 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "../hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+import toast from "react-hot-toast"; //Used to handle error and show a message
+import Button from "../Button";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal(); //created instance of useRegMod function of hooks folder
@@ -23,6 +27,8 @@ const RegisterModal = () => {
       password: "",
     },
   });
+
+  //SubmitHandler from react-hook-form ensures that every field(name email password) is filled or not if any one is unfilled it not let you submit the form
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
@@ -32,12 +38,80 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((error) => {
-        console.log(error);
+        toast.error("Something went wrong")
       })
       .finally(() => {
         setIsLoading(false);
       });
   };
+
+  const bodyContent = (
+  <div className="flex flex-col gap-4">
+    <Heading title="Welcome to AirBnb" subtitle="hello" center/>
+    <Input 
+    id="email" 
+    label="Email" 
+    disabled={isLoading} 
+    register={register} 
+    errors={errors} 
+    required
+    />
+    <Input 
+    id="name" 
+    label="Name" 
+    disabled={isLoading} 
+    register={register} 
+    errors={errors} 
+    required
+    />
+    <Input 
+    id="password"
+    type="password" 
+    label="Password" 
+    disabled={isLoading} 
+    register={register} 
+    errors={errors} 
+    required
+    />
+  </div>);
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline 
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}} 
+      />
+      <Button
+        outline 
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}} 
+      />
+      <div 
+        className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+      >
+        <p>Already have an account?
+          <span 
+            onClick={registerModal.onClose} 
+            className="
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
+            > Log in</span>
+        </p>
+      </div>
+      </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
@@ -46,6 +120,8 @@ const RegisterModal = () => {
       actionLabel="Continue"
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
     />
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/navbar/Navbar";
 import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const inter = Nunito({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ToasterProvider/>
         <RegisterModal />
         <Navbar />
         {children}
